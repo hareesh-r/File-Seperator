aeb38e42ffb7c6ca5b2cb70840ac54cf99db1918
HAREESH <53457844+hareesh-r@users.noreply.github.com>
2020-05-27 20:27:51 +0530

    Added Documentation and Some Extensions
    Added Tips to use and added Some more extensions that also with python automation .
    Also improved code readiblity

--- filesSeperator.py
+++ filesSeperator.py
@@ -1,27 +1,39 @@
 import os
 from pathlib import Path
+
+"""
+Download or copy and Paste this cose and save with .py extension and
+run inside the folder that you want to segregate
+"""
 subcategory={
-    'DOCUMENTS':['.pdf','.txt','.doc'.'rtf','.pptx','.docx'],
-    'AUDIO':['.mp3','.m4b','.m4a'],
-    'VIDEOS':['.mov','.avi','.mp4'],
-    'IMAGES':['.jpg','.jpeg','.raw','.png']
+
+    'DOCUMENTS':['.pdf','.txt','.doc','rtf','.pptx','.docx',
+                '.odt'],
+    'AUDIO':['.mp3','.m4b','.m4a','.webvtt','.cea-608/708',
+            '.dfxp', '.sami', '.scc', '.srt', '.ttml', '.3gpp'],
+    'VIDEOS':['.mov','.avi','.mp4''.3gp','.ogg', '.oga', '.ogv',
+                '.ogx','.wmv','.webm','.flv','.avi','.QuickTime',
+                '.hdv','.mxf','.mpeg-2','.ts','.wav','.lfx',
+                '.gfx''.vob'],
+    'IMAGES':['.jpg','.jpeg','.raw','.png','.tiff','.gif',],
+    'PROGRAMMING FILES':['.htm','.html','.cpp','.c','.py']
 }
-def pickCategory (value):
-    for name,ext in subcategory.items():
-        for exte in ext:
-            if exte == value:
-                return name
+def findTheCategory (value):
+    for filename,ext in subcategory.items():
+        for extension in ext:
+            if extension == value:
+                return filename
     return 'MISC'
 def organizeDir():
     for i in os.scandir():
         if i.is_dir():
             continue
-        filepath=Path(i)
-        filetype=filepath.suffix.lower()
-        diectory = pickCategory(filetype)
-        dirpath=Path(diectory)
-        if dirpath.is_dir() !=True:
-            dirpath.mkdir()
-        filepath.rename(dirpath.joinpath(filepath))
+        filePath=Path(i)
+        fileType=filePath.suffix.lower()
+        directory = findTheCategory(fileType)
+        directoryPath=Path(directory)
+        if directoryPath.is_dir() !=True:
+            directoryPath.mkdir()
+        filePath.rename(directoryPath.joinpath(filePath))
 
 organizeDir()
